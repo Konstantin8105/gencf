1c7bb11ad549be2890800de1a2f9a5753ea35cd4
Konstantin8105 <konstantin8105@gmail.com>
2018-11-09 23:16:49 +0300

    body

--- ast_to_html.go
+++ ast_to_html.go
@@ -1,3 +1,15 @@
 package main
 
 type S2html string
+
+func (s S2html) String() (out string) {
+
+	// 		fmt.Sprintf("func (value %s) ToHtml() (out string) {\n", name) +
+	// 		out
+	//
+	// 	// footer
+	// 	out += "	return\n"
+	// 	out += "}\n"
+
+	return
+}
--- ast_to_struct.go
+++ ast_to_struct.go
@@ -1,3 +1,16 @@
 package main
 
 type H2go string
+
+func (h H2go) String() (out string) {
+	// 	par = fmt.Sprintf("func (value *%s) FromHtml(r *http.Request) (err error) {\n", name) +
+	// 		"	et := errors.New(\"Errors of convert\")\n" +
+	// 		par
+	//
+	// 	par += "	if (et.IsError()){\n"
+	// 	par += "		return et\n"
+	// 	par += "	}\n"
+	// 	par += "	return\n"
+	// 	par += "}\n"
+	return
+}
